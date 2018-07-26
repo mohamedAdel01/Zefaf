6d22cb9277b57562e7e5fdad6f4ef7e8ae0ff59f
mohamedadel01 <mohamed7adel96@gmail.com>
2018-07-26 17:50:19 +0200

    make app's components

---
+++ client/src/components/app-Components/sub-Components/Navbar.vue
@@ -0,0 +1,24 @@
+<template>
+  <nav class="navbar bg-light navbar-light">
+    <a class="navbar-brand" @click="navigateTo('/app')">ZeFaF App</a>
+    <ul class="navbar-nav d-inline">
+      <li class="nav-item nav-link d-inline mr-3" @click="navigateTo('/app/checkout')">checkout</li>
+      <li class="nav-item nav-link d-inline" @click="navigateTo('/app/logout')">logout</li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+  methods: {
+    navigateTo(route) {
+      this.$router.push(route)
+    }
+  }
+}
+</script>
+
---
+++ client/src/components/app-Components/sub-Components/Sidebar.vue
@@ -0,0 +1,59 @@
+<template>
+  <aside class="sidenav">
+    <li @click="navigateTo('/app/accessories')">Accessories</li>
+    <li @click="navigateTo('/app/furniture')">Furniture</li>
+    <li @click="navigateTo('/app/hairstyle')">Hair style</li>
+    <li @click="navigateTo('/app/honeymoon')">Honeymoon</li>
+    <li @click="navigateTo('/app/hotels')">Hotels</li>
+    <li @click="navigateTo('/app/photographers')">Photographers</li>
+    <li @click="navigateTo('/app/weddingcakes')">Wedding Cakes</li>
+    <li @click="navigateTo('/app/weddingcars')">Wedding cars</li>
+    <li @click="navigateTo('/app/weddingclothes')">Wedding Clothes</li>
+    <li @click="navigateTo('/app/weddingflowers')">Wedding flowers</li>
+    <li @click="navigateTo('/app/weddinghalls')">Wedding Halls</li>
+    <li @click="navigateTo('/app/weddinginvitations')">Wedding Invitations</li>
+    <li @click="navigateTo('/app/weddingmusic')">Wedding Music</li>
+  </aside>
+</template>
+
+<script>
+export default {
+  methods: {
+    navigateTo(route) {
+      this.$router.push(route)
+    }
+  }
+}
+</script>
+
+<style>
+.sidenav {
+    height: 100%;
+    width: 160px;
+    position: fixed;
+    top: 56px;
+    left: 0;
+    background-color: #eee;
+    overflow-x: hidden;
+    padding-top: 20px;
+}
+
+.sidenav li {
+    padding: 3px 8px 3px 16px;
+    text-decoration: none;
+    font-size: 15px;
+    color: #818181;
+    display: block;
+}
+
+.sidenav li:hover {
+    color: skyblue;
+}
+
+.main {
+    margin-left: 160px; /* Same as the width of the sidenav */
+    font-size: 28px; /* Increased text to enable scrolling */
+    padding: 0px 10px;
+}
+</style>
+
