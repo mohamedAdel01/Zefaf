90930bea8c943886b0b31d640e4c32f5684a5232
mohamedadel01 <mohamed7adel96@gmail.com>
2018-08-06 03:41:08 +0200

    add multer to store files in public folder

--- server/src/index.js
+++ server/src/index.js
@@ -4,17 +4,32 @@ const mongoose = require('mongoose')
 const morgan = require('morgan')
 const bodyparser = require('body-parser')
 const cors = require('cors')
+const crypto = require('crypto')
+const multer = require('multer')
+const path = require('path')
 
 // connenting to database
 mongoose.connect('mongodb://localhost/zefaf')
 mongoose.Promise = global.Promise
 mongoose.connection.once('open', function(err) {
-    if (err) {
-      console.log('error found', err)
-    } else {
-      console.log('we are connected to database.')
-    }
-  })
+  if (err) {
+    console.log('error found', err)
+  } else {
+    console.log('we are connected to database.')
+  }
+})
+
+// Creating storage for files and images
+var storage = multer.diskStorage({
+  destination: '../public/uploads/',
+  filename: function (req, file, cb) {
+    crypto.pseudoRandomBytes(16, function (err, raw) {
+      cb(null, raw.toString('hex') + Date.now() + path.extname(file.originalname))
+    })
+  }
+})
+
+module.exports.upload = multer({ storage: storage }).single('file')
 
 // connecting to server
 const app = express()
@@ -23,6 +38,9 @@ const app = express()
 app.use(morgan('dev'))
 app.use(bodyparser.json())
 app.use(cors())
+
+// Public Folder
+app.use(express.static('../public'))
 
 // routers
 require('./router/routes')(app)
--- server/src/router/routes.js
+++ server/src/router/routes.js
@@ -1,10 +1,22 @@
 const authPolices = require('../../policies/authPolicies')
 const authcontrolles = require('../../controlles/authcontrolles')
 const dataControlls = require('../../controlles/dataControlls')
+const upload = require('../index').upload
 
 module.exports = (app) => {
-    app.post('/register', authPolices.register, authcontrolles.register)
-    app.post('/login', authcontrolles.login)
+  app.post('/register', authPolices.register, authcontrolles.register)
+  app.post('/login', authcontrolles.login)
 
-    app.get('/details/:sort', dataControlls.show)
+  app.get('/details/:sort', dataControlls.show)
+
+  // test ==========================================================
+  app.post('/saveImage', (req, res) => {
+    upload(req, res, (err) => {
+      if (err) {
+        console.log(err)
+        return
+      }
+      res.send(req.file)
+    })
+  })
 }
