e5a8d0c82f719a483cdc5627c8abc2e491140b24
mohamedadel01 <mohamed7adel96@gmail.com>
2018-08-08 21:17:45 +0200

    add vue-i18n lib to make translation for the site

---
+++ client/src/local/languageData.js
@@ -0,0 +1,15 @@
+// Ready translated locale messages
+const messages = {
+  en: {
+    message: {
+      hello: 'hello world'
+    }
+  },
+  ar: {
+    message: {
+      hello: 'بسم الله'
+    }
+  }
+}
+
+export default messages
--- client/src/main.js
+++ client/src/main.js
@@ -1,17 +1,28 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import VueI18n from 'vue-i18n'
 import App from './App'
 import router from './router'
 import {store} from './store/store.js'
+import messages from './local/languageData'
 
 Vue.config.productionTip = false
+
+Vue.use(VueI18n)
+
+// Create VueI18n instance with options
+const i18n = new VueI18n({
+  locale: 'ar', // set locale
+  messages, // set locale messages
+})
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  store:store,
+  store,
+  i18n,
   components: { App },
   template: '<App/>'
 })
